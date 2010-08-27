4fabe69c934914be24d76c2890dfb6a747234eb3
steve.garon <steve.garon@19c5bbfb-c0c7-3e07-fbe0-d9d3ca4c37c7>
2010-08-27 02:41:47 +0000

    REMOVED: Unused import

--- branches/br_1.0_market/src/org/jared/synodroid/ds/DetailActivity.java
+++ branches/br_1.0_market/src/org/jared/synodroid/ds/DetailActivity.java
@@ -57,7 +57,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
