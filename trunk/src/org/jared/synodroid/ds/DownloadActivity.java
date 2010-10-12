13657be5e7f63462fe30a455891cb3e316bfa1bb
eric.taix <eric.taix@19c5bbfb-c0c7-3e07-fbe0-d9d3ca4c37c7>
2010-10-12 16:48:59 +0000

    WLAN discovering using ZeroConf protocol

--- trunk/src/org/jared/synodroid/ds/DownloadActivity.java
+++ trunk/src/org/jared/synodroid/ds/DownloadActivity.java
@@ -44,7 +44,7 @@ import org.jared.synodroid.ds.action.ShowDetailsAction;
 import org.jared.synodroid.ds.action.TaskActionMenu;
 import org.jared.synodroid.ds.view.adapter.ActionAdapter;
 import org.jared.synodroid.ds.view.adapter.TaskAdapter;
-import org.jared.synodroid.ds.view.wizard.WizardServerFactory;
+import org.jared.synodroid.ds.view.wizard.ServerWizard;
 
 import android.app.Activity;
 import android.app.AlertDialog;
