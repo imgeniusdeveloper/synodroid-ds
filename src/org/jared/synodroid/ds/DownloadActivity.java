bea466e187a1070fb8278fda0919fe75a4e192c4
steve.garon <steve.garon@19c5bbfb-c0c7-3e07-fbe0-d9d3ca4c37c7>
2010-12-09 00:07:37 +0000

    Fixed Out of memory error while downloading original Fixed null pointer exception on task detail status Fixed null pointer exception on download original file

--- src/org/jared/synodroid/ds/DownloadActivity.java
+++ src/org/jared/synodroid/ds/DownloadActivity.java
@@ -11,7 +11,6 @@ package org.jared.synodroid.ds;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
