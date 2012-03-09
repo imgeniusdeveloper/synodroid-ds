99659d36af5e7d700e135f84d23681b4356e4f23
steve.garon <steve.garon@19c5bbfb-c0c7-3e07-fbe0-d9d3ca4c37c7>
2012-03-09 16:38:10 +0000

    Added update searchengine button in preference

--- src/org/jared/synodroid/ds/DownloadActivity.java
+++ src/org/jared/synodroid/ds/DownloadActivity.java
@@ -105,7 +105,7 @@ public class DownloadActivity extends SynodroidActivity implements Eula.OnEulaAg
 	private static final String PREFERENCE_GENERAL = "general_cat";
 	private static final String PREFERENCE_SEARCH_SOURCE = "general_cat.search_source";
 	private static final String PREFERENCE_SEARCH_ORDER = "general_cat.search_order";
-	private static final String TORRENT_SEARCH_URL_DL = "http://code.google.com/p/transdroid-search/downloads/list";
+	private static final String TORRENT_SEARCH_URL_DL = "http://transdroid.org/latest-search";
 	private static final String TORRENT_SEARCH_URL_DL_MARKET = "market://details?id=org.transdroid.search";
 	private static final String SYNO_PRO_URL_DL_MARKET = "market://details?id=com.bigpupdev.synodroid";
 	
